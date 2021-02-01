22a37e4059eb84692254da8558984578f926675c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-01 13:24:05 +0200

    refactoring-folders-files

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+
 /*--------------------Explanation---------------------- */
 // creating a new class
 // our goal is to get access to the template and to the form as well as to the div in our html
@@ -22,17 +23,9 @@
 // to get access in that element we need to add some properties in this class
 // so we adding more fields up there
 
-///<reference path="models/drag-drop.ts"/>
-///<reference path="models/project.ts"/>
-///<reference path="state/project-state.ts"/>
-///<reference path="util/validation.ts"/>
-///<reference path="decorators/autobind.ts"/>
-///<reference path="components/project-input.ts"/>
-///<reference path="components/project-list.ts"/>
+import { ProjectInput } from './components/project-input.js';
+import { ProjectList } from './components/project-list.js';
 
-namespace App{
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
-}
-
